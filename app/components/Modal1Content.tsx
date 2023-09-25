427384235e8b8300f8fb04ccab6dd4276f599f51
Rahil Ansari <12849916+rahilansari261@users.noreply.github.com>
2023-09-25 17:56:43 +0000

    modal1content func removed

--- app/components/Modal1Content.tsx
+++ app/components/Modal1Content.tsx
@@ -3,9 +3,7 @@ const Modal1Content = ({
   closeFirstModal,
   openSecondModel,
 }: any) => {
-  const handleSubmit = () => {
-    closeFirstModal();
-  };
+
 
   return (
     <div className="p-12 text-[#212427] bg-white rounded-lg  ">
@@ -31,7 +29,10 @@ const Modal1Content = ({
         <div className="flex items-start justify-end gap-4 pt-12">
           <button onClick={openSecondModel}>Cancel</button>
 
-          <button className=" btn col-span-3 flex !gap-[6px] !rounded !border-none bg-gradient-to-r from-[#4576F6] to-[#0C49E7] px-6 py-3 text-lg  !font-medium !capitalize text-[#fff] hover:bg-[#380Eff]">
+          <button
+            onClick={closeFirstModal}
+            className=" btn col-span-3 flex !gap-[6px] !rounded !border-none bg-gradient-to-r from-[#4576F6] to-[#0C49E7] px-6 py-3 text-lg  !font-medium !capitalize text-[#fff] hover:bg-[#380Eff]"
+          >
             Add User
           </button>
         </div>
